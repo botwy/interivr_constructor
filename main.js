c4542d851f7057a1bef7413d3f8f6e211898e4a7
botwy <botwy@mail.ru>
2018-12-04 23:28:30 +0300

    delete saga

--- main.js
+++ main.js
@@ -3,18 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import createSagaMiddleware from "redux-saga";
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import App from './konva-transformer/AppKonva';
 import reducer from './konva-transformer/reducer';
-import {watchUpdateModel} from "./konva-transformer/saga/index";
-const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware)),
+  composeWithDevTools(applyMiddleware(thunk)),
   );
-sagaMiddleware.run(watchUpdateModel);
 
 ReactDOM.render(
   <Provider store={store}>
